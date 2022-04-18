e77b4accb92cac6f243ebd20529651d5dc5de385
Shafaitul-jannat <shafaitul.jannat@gmail.com>
2022-04-19 03:12:26 +0600

    about added

--- src/Pages/HomePage/Footer/Footer.js
+++ src/Pages/HomePage/Footer/Footer.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import './footer.css';
 
 const Footer = () => {
     const today = new Date();
     const year = today.getFullYear();
     return (
-        <footer className='text-center mt-5'>
-            <p><small>Copyright @ {year}</small></p>
-        </footer>
+
+        <footer className="footer-design" >
+            <div>
+                <h3><span className="text-success">Shipa's</span> Photography</h3>
+                <p><small>Copyright @ {year}</small></p>
+                <p>All right reserved</p>
+
+            </div>
+            <div>
+                <i className="fab fa-instagram-square"></i> <i className="fab fa-dribbble-square"></i>
+                <i className="fab fa-twitter-square"></i> <i className="fab fa-youtube-square"></i>
+            </div>
+        </footer >
+
+
+
+
+
     );
 };
 
